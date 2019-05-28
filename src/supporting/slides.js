3a8bf7a55e6b7f3baf8ed3214f09dc1f038f2786
Guidogl <gvidasglebas@gmail.com>
2019-05-28 20:36:03 +0300

    Fix navigation and progress

---
+++ src/supporting/slides.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+
+export default [
+  {
+    img: img1,
+    title: "Ohare Global Terminal",
+    content: (
+      <p>
+        Calatrava’s plan includes the global terminal and a business complex
+        with formal gardens that would remake the present site of parking
+        facilities next to the terminals. The terminal is formed with a pointed
+        form featuring a white roof overhang. The terminal’s interior features
+        Calatrava's white-on-white uniform surfaces with plenty of natural
+        light.
+      </p>
+    )
+  },
+  {
+    img: img2,
+    title: "Beijing Daxing International Airport",
+    content: (
+      <p>
+        The terminal building was designed by British architects Zaha Hadid
+        Architects and French planners ADPI, and executed by the Beijing
+        Institute of Architectural Design (BIAD). It consists of a central hub
+        with six curved spokes.[15] Hong Kong design studio Lead 8 was appointed
+        as lead designer of the commercial terminal in 2018.
+      </p>
+    )
+  },
+  {
+    img: img3,
+    title: "Kuwait International Airport",
+    content: (
+      <p>
+        Kuwait International Airport is planned to significantly increase
+        capacity and establish a new regional air hub in the Gulf - these
+        strategic aims will be matched by a state-of-the-art terminal building,
+        which will provide the highest levels of comfort for passengers and will
+        set a new environmental benchmark for airport buildings. Its design is
+        rooted in a sense of place, responsive to the climate of one of the
+        hottest inhabited environments on earth and inspired by local forms and
+        materials.
+      </p>
+    )
+  }
+];
